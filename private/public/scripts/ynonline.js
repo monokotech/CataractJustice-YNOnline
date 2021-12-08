0b80e3fbf6a13cd58b13403758f2d4e0813725e6
Eduard Zubovych <Sw33tH34rt@protonmail.ch>
2021-12-08 19:38:15 +0200

    game path fix

--- private/public/scripts/ynonline.js
+++ private/public/scripts/ynonline.js
@@ -6,7 +6,7 @@ let YNOnline = {Network:{}};
 if(urlParams.get("game"))
 	Module.EASYRPG_GAME = urlParams.get("game");
 else
-	Module.EASYRPG_GAME = window.location.pathname.replace('/', '');
+	Module.EASYRPG_GAME = window.location.pathname.replaceAll('/', '');
 
 //comment this to stop pings
 ////////
