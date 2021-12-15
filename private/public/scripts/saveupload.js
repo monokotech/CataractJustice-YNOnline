adfa973dc78d2b95a9bd92c910852d56e85edb69
Eduard Zubovych <Sw33tH34rt@protonmail.ch>
2021-12-16 01:06:22 +0200

    added save download option

--- private/public/scripts/saveupload.js
+++ private/public/scripts/saveupload.js
@@ -1,3 +1,4 @@
+let saveOptions = document.getElementById("saveOptions");
 let wantUploadSaveButton = document.getElementById("wantUploadSaveButton");
 let uploadSaveButton = document.getElementById("uploadSaveButton");
 let uploadSaveContainet = document.getElementById("uploadSaveContainer");
@@ -6,12 +7,12 @@ let uploadSaveSlotInput = document.getElementById("uploadSaveSlotInput");
 
 wantUploadSaveButton.onclick = function() {
 	uploadSaveContainet.style.display = "block";
-	wantUploadSaveButton.style.display = "none";
+	saveOptions.style.display = "none";
 }
 
 uploadSaveButton.onclick = async function() {
 	uploadSaveContainet.style.display = "none";
-	wantUploadSaveButton.style.display = "block";
+	saveOptions.style.display = "block";
 	let saveFileBuffer = new Uint8Array(await uploadSaveFileInput.files[0].arrayBuffer());
   	FS.writeFile("Save/Save" + (uploadSaveSlotInput.value < 10 ? "0" : "") + uploadSaveSlotInput.value + ".lsd", saveFileBuffer);
 	FS.syncfs(false, function(){});
