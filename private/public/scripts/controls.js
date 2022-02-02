1bb4e9c487884b04f73dc7629fa5112597fe3697
Eduard Zubovych <Sw33tH34rt@protonmail.ch>
2022-02-02 12:03:50 +0200

    add tab button to mobile controls for chat access, hide html chat on mobile

--- private/public/scripts/controls.js
+++ private/public/scripts/controls.js
@@ -12,37 +12,40 @@ let additionalControls = {
 }
 
 let debugControls = [
-	{name: "░", data_key_code: "17"},
-	{name: "F", data_key_code: "70"},
-	{name: "G", data_key_code: "71"}
-]
+	{name: "░", data_key: "Ctrl", data_key_code: "17"},
+	{name: "F", data_key: "F", data_key_code: "70"},
+	{name: "G", data_key: "G", data_key_code: "71"}
+];
+
+let npad;
+
+function addControl(key) {
+	let keynode = document.createElement("div");
+	keynode.id = key.name + "_touch_key";
+	keynode.innerText = key.name;
+	keynode.dataset["key"] = key.data_key;
+	keynode.dataset["keyCode"] = key.data_key_code;
+	npad.appendChild(keynode);
+}
+
 
 function initControls() {
-	let npad = document.getElementById("npad");
+	npad = document.getElementById("npad")
 
-	console.log(gameName);
-	
 	if(urlParams.get("test-play") === '') {
 		for(let key of debugControls) {
-			let keynode = document.createElement("div");
-			keynode.id = key.name;
-			keynode.innerText = key.name;
-			keynode.dataset["key"] = key.data_key;
-			keynode.dataset["keyCode"] = key.data_key_code;
-			npad.appendChild(keynode);
+			addControl(key);
 		}
 	}
 
 	if(additionalControls[gameName]) {
 		for(let key of additionalControls[gameName]) {
-			let keynode = document.createElement("div");
-			keynode.id = key.name;
-			keynode.innerText = key.name;
-			keynode.dataset["key"] = key.data_key;
-			keynode.dataset["keyCode"] = key.data_key_code;
-			npad.appendChild(keynode);
+			addControl(key);
 		}
 	}
+
+	//add chat button
+	addControl({name: "💬", data_key: "Tab", data_key_code: "9"});
 }
 
 initControls();
